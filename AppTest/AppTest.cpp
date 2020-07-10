93c4a1ed80102859514761f76b7e5d38e70a8957
Vladmir <roldugin.67@gmail.com>
2020-07-10 09:16:14 +0300

    Добавьте файлы проекта.

---
+++ AppTest/AppTest.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <string>
+using namespace std;
+
+class Student
+{
+public:
+
+
+private:
+
+};
+
+
+int main()
+{
+    setlocale(LC_ALL, "ru");
+
+    int a;
+    cout << " 1 – Добавить позиции\n";
+    cout << " 2 – Удалить позиции\n";
+    cout << " 3 – Вывести список позиций\n";
+    cout << " 4 – Выполнять текстовый поиск позиции\n";
+    cout << " 5 – Выход\n\n";
+
+
+    cout << "Select: ";
+    cin >> a;
+
+
+}
